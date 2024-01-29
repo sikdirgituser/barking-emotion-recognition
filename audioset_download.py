8ac25fddf38ca1aae172bd11e7d792ca00e27ca5
Pascal Albisser <pascal@Pascals-MacBook-Pro.local>
2024-01-29 08:16:38 +0100

    add start/stop to cut audio file

--- audioset_download.py
+++ audioset_download.py
@@ -4,8 +4,9 @@ from pytube import YouTube
 from pydub import AudioSegment
 import pandas as pd
 
-# Replace 'your_csv_file_path.csv' with the actual path to your CSV file
+# read in the dataset
 csv_file_path = 'data/dataset.csv'
+
 # Read the CSV file into a DataFrame, skipping lines starting with '#'
 df = pd.read_csv(csv_file_path)
 
@@ -18,7 +19,12 @@ df['stop'] = df['stop'].astype(int)
 output_path = 'data/audioset_audios'
 
 def download_audio(row, output_path):
+
     video_id = row['ytid']
+
+    if os.path.isfile(f"{output_path}/{row['ytid']}.mp3"):
+        print(f"video {row['ytid']} already downloaded, skipping.")
+        return None
 
     try:
         # Video erhalten
@@ -27,26 +33,34 @@ def download_audio(row, output_path):
         # Audio-Stream auswählen
         audio_stream = video.streams.filter(only_audio=True).first()
 
-        # Video herunterladen und schneiden
-        audio_stream.download(output_path, filename=f"{video_id}_cut.mp3")
+        print(f"download {video_id}")
+
+        # Video herunterladen
+        audio_stream.download(output_path, filename=f"{video_id}.mp3")
 
     except Exception as e:
-        print(f"Fehler beim Herunterlanden von Video {video_id}: {e}")
+        print(f"Fehler beim Herunterladen von Video {video_id}: {e}")
 
-def cut_audio(row, output_path):
+def cut_audio(row, output_path, full_audio_path):
     video_id = row['ytid']
     start_time = int(row['start'])
     end_time = int(row['stop'])
 
+    export_path = f"{output_path}/{video_id}_{start_time}_{end_time}_cut.mp3"
+
+    if os.path.isfile(export_path):
+        print(f"video {export_path} already cut, skipping.")
+        return None
+
     try:
-        audio_file_path = f"{output_path}/{video_id}_cut.mp3"
+        audio_file_path = f"{full_audio_path}/{video_id}.mp3"
 
         # Audio öffnen und schneiden
         audio = AudioSegment.from_file(audio_file_path)
         cut_audio = audio[start_time * 1000:end_time * 1000]
 
         # Geschnittene Audio speichern
-        cut_audio.export(f"{output_path}/{video_id}_cut.mp3", format="mp3")
+        cut_audio.export(export_path, format="mp3")
 
     except Exception as e:
         print(f"Fehler beim Verarbeiten von Video {video_id}: {e}")
@@ -54,12 +68,10 @@ def cut_audio(row, output_path):
 
 def process_audio(row, output_path):
 
-    if os.path.isfile(f"{output_path}/{row['ytid']}_cut.mp3"):
-        print(f"video {row['ytid']} already downloaded, skipping.")
-        return None
+    download_audio(row, f"{output_path}/full_video")
+    cut_audio(row, output_path, f"{output_path}/full_video")
 
-    download_audio(row, output_path)
-    cut_audio(row, output_path)
 
-# download & cut audios
-df.apply(lambda row: process_audio(row, output_path), axis=1)
+if __name__ == "__main__":
+    # download & cut audios
+    df.apply(lambda row: process_audio(row, output_path), axis=1)
